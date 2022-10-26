d10f3a222e0b22db00886f922ffcb3ec3ecb26dc
rasyidev <habib.rasyid11@gmail.com>
2022-10-26 19:31:09 +0700

    Create function to generate user created response data

--- models/user.go
+++ models/user.go
@@ -11,3 +11,19 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type UserCreatedResponseData struct {
+	ID       string `json:"user" gorm:"primaryKey"`
+	Age      uint   `json:"age"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
+
+func (u *User) ToUserCreatedResponse() UserCreatedResponseData {
+	return UserCreatedResponseData{
+		ID:       u.ID,
+		Age:      u.Age,
+		Email:    u.Email,
+		Username: u.Username,
+	}
+}
