66ade096f24925c509a3cbe4646fb549c650f3a0
rasyidev <habib.rasyid11@gmail.com>
2022-10-26 15:25:04 +0700

    Add JSON response for CreateUser handler

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,20 +1,26 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
 	"github.com/rasyidev/dts-h8-mygram/database"
 	"github.com/rasyidev/dts-h8-mygram/models"
 )
 
-func CreateUser(ctx gin.Context) {
+func CreateUser(ctx *gin.Context) {
 	db := database.NewDB()
-	user := models.User{}
+	id, _ := uuid.NewV4()
+
+	user := models.User{ID: id.String(), CreatedAt: time.Now(), UpdatedAt: time.Now()}
 	if err := ctx.ShouldBindJSON(&user); err != nil {
 		log.Fatal(err.Error())
 	}
+	fmt.Println(user)
 
 	if err := db.Create(&user).Error; err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
@@ -22,4 +28,10 @@ func CreateUser(ctx gin.Context) {
 			"message": err.Error(),
 		})
 	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status":  http.StatusCreated,
+		"message": "Success creating new user",
+		"data":    user,
+	})
 }
