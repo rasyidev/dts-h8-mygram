3781cea0727dbd1db8015837e1a5c5267b6b67e8
rasyidev <habib.rasyid11@gmail.com>
2022-10-26 14:09:05 +0700

    modified:   models/photo.go

--- models/photo.go
+++ models/photo.go
@@ -1,10 +1,14 @@
 package models
 
+import "time"
+
 type Photo struct {
-	ID       string `json:"id" gorm:"primaryKey"`
-	Title    string `json:"title"`
-	Caption  string `json:"caption"`
-	PhotoUrl string `json:"photo_url"`
-	UserID   string `json:"user_id"`
-	User     User
+	ID         string `json:"id" gorm:"primaryKey"`
+	Title      string `json:"title"`
+	Caption    string `json:"caption"`
+	PhotoUrl   string `json:"photo_url"`
+	UserID     string `json:"user_id"`
+	User       User
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
 }
