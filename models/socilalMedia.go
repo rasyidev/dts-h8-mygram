7cd6025ed9b24a2e4fbc8f1479f625f2a830c479
rasyidev <habib.rasyid11@gmail.com>
2022-10-27 07:15:49 +0700

    Add validation criteria

--- models/socilalMedia.go
+++ models/socilalMedia.go
@@ -4,8 +4,8 @@ import "time"
 
 type SocialMedia struct {
 	ID             string `json:"id" gorm:"primaryKey"`
-	Name           string `json:"name"`
-	SocialMediaUrl string `json:"social_media_url"`
+	Name           string `json:"name" validate:"required"`
+	SocialMediaUrl string `json:"social_media_url" validate:"required,url"`
 	UserID         string `json:"user_id"`
 	User           User
 	CreatedAt      time.Time `json:"created_at"`
