b7a2bf258cc5aa8b40cd9a1330137b1e18d914b2
rasyidev <habib.rasyid11@gmail.com>
2022-10-27 07:17:07 +0700

    Add validation criteria

--- models/photo.go
+++ models/photo.go
@@ -4,9 +4,9 @@ import "time"
 
 type Photo struct {
 	ID        string `json:"id" gorm:"primaryKey"`
-	Title     string `json:"title"`
+	Title     string `json:"title" validate:"required"`
 	Caption   string `json:"caption"`
-	PhotoUrl  string `json:"photo_url"`
+	PhotoUrl  string `json:"photo_url" validate:"required,url"`
 	UserID    string `json:"user_id"`
 	User      User
 	CreatedAt time.Time `json:"created_at"`
