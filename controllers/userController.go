6c4786c30941df64ad1a5f835301175764e5b290
rasyidev <habib.rasyid11@gmail.com>
2022-10-27 07:08:29 +0700

    Use validation

--- controllers/userController.go
+++ controllers/userController.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofrs/uuid"
 	"github.com/rasyidev/dts-h8-mygram/database"
 	"github.com/rasyidev/dts-h8-mygram/models"
@@ -19,6 +20,15 @@ func CreateUser(ctx *gin.Context) {
 	user := models.User{ID: id.String(), CreatedAt: time.Now(), UpdatedAt: time.Now()}
 	if err := ctx.ShouldBindJSON(&user); err != nil {
 		log.Fatal(err.Error())
+	}
+
+	// validate the request body
+	if err := validator.New().Struct(user); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": "Validation error: " + err.Error(),
+		})
+		return
 	}
 
 	// hashing the password
@@ -40,4 +50,27 @@ func CreateUser(ctx *gin.Context) {
 		"message": "Success creating new user",
 		"data":    user.ToUserCreatedResponse(),
 	})
+}
+
+func Login(ctx *gin.Context) {
+	db := database.NewDB()
+	requestBody := models.User{}
+
+	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
+		log.Fatal("Error binding to user data: ", err.Error())
+	}
+
+	encryptedPass, _ := bcrypt.GenerateFromPassword([]byte(requestBody.Password), 1)
+
+	if err := db.Where("email=? AND password=?", requestBody.Email, string(encryptedPass)).Limit(1).Error; err != nil {
+		log.Fatal("Email not found: ", err.Error())
+		ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusNotFound,
+			"message": "Email not found: " + err.Error(),
+		})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"token": "abcdefghijklmnopqrstuvwxyz",
+		})
+	}
 }
