ef5439c6c8a92af6384149fb52f874a4ef321c5f
rasyidev <habib.rasyid11@gmail.com>
2022-10-28 07:15:23 +0700

    modified:   controllers/userController.go

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,20 +1,24 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofrs/uuid"
 	"github.com/rasyidev/dts-h8-mygram/database"
+	"github.com/rasyidev/dts-h8-mygram/helper"
 	"github.com/rasyidev/dts-h8-mygram/models"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func CreateUser(ctx *gin.Context) {
 	db := database.NewDB()
+
 	id, _ := uuid.NewV4()
 
 	user := models.User{ID: id.String(), CreatedAt: time.Now(), UpdatedAt: time.Now()}
@@ -55,22 +59,82 @@ func CreateUser(ctx *gin.Context) {
 func Login(ctx *gin.Context) {
 	db := database.NewDB()
 	requestBody := models.User{}
+	user := models.User{}
 
 	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
 		log.Fatal("Error binding to user data: ", err.Error())
 	}
 
-	encryptedPass, _ := bcrypt.GenerateFromPassword([]byte(requestBody.Password), 1)
+	// encryptedPass, _ := bcrypt.GenerateFromPassword([]byte(requestBody.Password), 1)
+	// fmt.Println("encryptedPass: ", string(encryptedPass))
 
-	if err := db.Where("email=? AND password=?", requestBody.Email, string(encryptedPass)).Limit(1).Error; err != nil {
-		log.Fatal("Email not found: ", err.Error())
-		ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{
-			"status":  http.StatusNotFound,
-			"message": "Email not found: " + err.Error(),
-		})
+	if err := db.Model(&user).Where("email=?", requestBody.Email).Limit(1).Scan(&user).Error; err != nil {
+		log.Println("error binding user data")
 	} else {
+		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(requestBody.Password)); err != nil {
+			log.Println("Email or password not match: ", err.Error())
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"status":  http.StatusUnauthorized,
+				"message": "Email or password not match " + err.Error(),
+			})
+			return
+		}
+
+		fmt.Println("email and password match: ", user.Email, user.Password)
+		token := helper.GenerateToken(user.ID, user.Email)
 		ctx.JSON(http.StatusOK, gin.H{
-			"token": "abcdefghijklmnopqrstuvwxyz",
+			"token": token,
 		})
 	}
+}
+
+// UpdateUser update email and username of the logged-in user
+func UpdateUser(ctx *gin.Context) {
+	db := database.NewDB()
+	requestBody := models.User{UpdatedAt: time.Now()}
+	user := models.User{}
+	userData := ctx.MustGet("userData").(jwt.MapClaims)
+	fmt.Println("userData: ", userData)
+	contentType := helper.GetContentType(ctx)
+	fmt.Println("contentType: ", contentType)
+
+	ctx.ShouldBindJSON(&requestBody)
+
+	if err := db.Model(&models.User{}).Where("id=?", userData["id"]).Updates(requestBody).Scan(&user).Error; err != nil {
+		log.Println("Error updating user: ", err.Error())
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": "Error updating user " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "Success updating data",
+		"data":    user.ToUserUpdatedResponse(),
+	})
+}
+
+// UpdateUser update email and username of the logged-in user
+func DeleteUser(ctx *gin.Context) {
+	db := database.NewDB()
+	userData := ctx.MustGet("userData").(jwt.MapClaims)
+	fmt.Println("userData: ", userData)
+	contentType := helper.GetContentType(ctx)
+	fmt.Println("contentType: ", contentType)
+
+	if err := db.Where("id=?", userData["id"]).Delete(&models.User{}).Error; err != nil {
+		log.Println("Error deleting user: ", err.Error())
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": "Error deleting user " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "Your account has been successfully deleted",
+	})
 }
